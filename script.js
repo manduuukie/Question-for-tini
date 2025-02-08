4bd05308fe5426dcc9d28e444eb960a33331de6a
manduuukie <angieou890@gmail.com>
2025-02-08 00:01:21 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function flashRainbowColors(callback) {
 }
 
 // Function to display the muñeñoo.gif initially
-function displaymuñeñoo() {
+function displayMuñeñoo() {
     // Get the container where the image will be displayed
     var imageContainer = document.getElementById('image-container');
     // Create a new Image element for muñeñoo
@@ -49,7 +49,7 @@ function displaymuñeñoo() {
     // Set the source (file path) for the muñeñoo image
     muñeñooImage.src = 'muñeñoo.gif'; // Assuming the muñeñoo image is named "muñeñoo.gif"
     // Set alternative text for the image (for accessibility)
-    muñeñooImage.alt = 'muñeñoo';
+    muñeñooImage.alt = 'Muñeñoo';
     // When the muñeñoo image is fully loaded, add it to the image container
     muñeñooImage.onload = function() {
         imageContainer.appendChild(muñeñooImage);
@@ -63,11 +63,11 @@ function displayBobi() {
     // Get the container where the image will be displayed
     var imageContainer = document.getElementById('image-container');
     // Create a new Image element for the bobi
-    var bobitImage = new Image();
+    var bobiImage = new Image();
     // Set the source (file path) for the bobi image
     bobiImage.src = 'bobi.gif'; // Assuming the bobi image is named "bobi.gif"
     // Set alternative text for the image (for accessibility)
-    bobiImage.alt = 'bobi';
+    bobiImage.alt = 'Bobi';
     // When the bobi image is fully loaded, add it to the image container
     bobiImage.onload = function() {
         imageContainer.appendChild(bobiImage);
@@ -77,4 +77,4 @@ function displayBobi() {
 }
 
 // Display the muñeñoo.gif initially
-displaymuñeñoo();
+displayMuñeñoo();
